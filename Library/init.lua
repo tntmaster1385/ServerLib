e05415cb2ce06cc1a9eadcec6f377e18c658fc71
Server <121733356+tntmaster1385@users.noreply.github.com>
2024-11-07 17:20:28 -0600

    Update init.lua

--- Library/init.lua
+++ Library/init.lua
@@ -13,12 +13,12 @@ Library.Instances = {}
 
 local Root = script.Parent
 
-local Fade = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/Fade.lua"))
-local SnapDragon = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/SnapDragon/init.lua"))
-local FitToSize = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/FitToSize.lua"))
-local CircleClick = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/CircleClick.lua"))
-local Intro = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/Intro.lua"))
-local States = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/States.lua"))
+local Fade = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/Fade.lua"))()
+local SnapDragon = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/SnapDragon/init.lua"))()
+local FitToSize = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/FitToSize.lua"))()
+local CircleClick = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/CircleClick.lua"))()
+local Intro = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/Intro.lua"))()
+local States = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/States.lua"))()
 
 function Library:Tween(Object, Info, Goal)
 	local Tween = TweenService:Create(Object, Info, Goal)
